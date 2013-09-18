925b764095f00c81ce235d40fac39c120639a172
Erica Tafoya <emtafoya@gmail.com>
2013-09-17 21:57:58 -0600

    Update index.php and add p1css.css

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Welcome to P1</title>
+<title>Erica Tafoya Bio</title>
+<link rel="stylesheet" href="/p1css.css" />
 </head>
 
 <body>
-<h1>Welcome to P1!</h1>
+<h1>Erica Tafoya</h1>
+<p>I was born in San Francisco, CA and raised in Montana. I went to school at Seton Hall University in New Jersey. After receiving a master's degree in Diplomacy & International Relations, I moved to Washington, DC. While there, I got married and had a son. In 2010, we moved to Manchester, UK, where we spent a year and a half. </p>
+<p>I am now happily settled in Denver, CO, working as a front-end web developer. My real love (in addition to my son), however, is Human Design. I am studying to become a Human Design anaylst in the hopes of combining it with some form of therapy.</p>
+<p>My code editor is Dreamweaver.</p>
 </body>
 </html>
